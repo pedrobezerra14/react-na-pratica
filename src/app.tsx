8b58b7dc74cb75f9a855722f992884774276cea3
Pedro Henrique <pedro.bezerra14@aluno.pb.gov.br>
2024-02-20 14:55:10 -0300

    error -> Pagination
    Type '{}' is missing the following properties from type 'PaginationProps': pages, items, page

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,26 @@
-import { Plus, Search, FileDown } from 'lucide-react'
+import { Plus, Search, FileDown, MoreHorizontal } from 'lucide-react'
 import { Header } from './components/header'
 import { Tabs } from './components/tabs'
 import { Button } from './components/ui/button'
 import { Control, Input } from './components/ui/input'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from './components/ui/table'
+import { Pagination } from './components/pagination'
+
+export interface TagResponse {
+  first: number
+  prev: number | null
+  next: number
+  last: number
+  pages: number
+  items: number
+  data: Tag[]
+}
+
+export interface Tag {
+  title: string
+  amountOfVideos: number
+  id: string
+}
 
 export function App() {
   return (
@@ -20,7 +38,7 @@ export function App() {
          </Button>
        </div>
        <div className='flex items-center justify-between'>
-        <Input variant="filter">
+        <Input variant= 'filter'>
           <Search className='size-3' />
           <Control placeholder='Search tags...' />
         </Input>
@@ -30,6 +48,43 @@ export function App() {
           Export
         </Button>
        </div>
+      
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead></TableHead>
+            <TableHead>Tags</TableHead>
+            <TableHead>Amount of videos</TableHead>
+            <TableHead></TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {Array.from({ length: 10 }).map((value, index) => {
+            return (
+            <TableRow key={index}>
+              <TableCell>
+              </TableCell>
+              <TableCell>     
+                <div className='flex flex-col gap-0.5'>
+                  <span className='font-medium'>React</span>
+                  <span className='text-xs text-zinc-500'>12345-982324-212255-51247-12112</span>
+                </div>  
+              </TableCell>
+              <TableCell className='text-zinc-300'>
+                13 video(s)
+              </TableCell>
+              <TableCell className='text-right'>
+              <Button size='icon'>
+                <MoreHorizontal className='size-4'/>
+                </Button>
+              </TableCell>
+            </TableRow>
+              )
+          })}
+        </TableBody>
+      </Table>
+
+      <Pagination />
       </main>
     </div>
   )

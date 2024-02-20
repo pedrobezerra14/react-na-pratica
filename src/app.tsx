152ec7cb5196c839caafc2dafee9e9e64f8c80ea
Pedro Henrique <pedro.bezerra14@aluno.pb.gov.br>
2024-02-20 07:19:22 -0300

    add:logo

--- src/app.tsx
+++ src/app.tsx
@@ -8,8 +8,8 @@ export function App() {
         tabs
       </div>
       <main className="max-w-6xl mx-auto space-y-5">
+      <div className='flex items-center gap-3'>
         <h1 className="text-xl font-bold">Tags</h1>
-        <div className='flex items-center gap-3'>
         <button className='inline-flex items-center gap-1.5 text-xs bg-teal-300 text-teal-950 font-medium rounded-full px-1.5 py-1'>
           <Plus className='size-3'/>
           Create New
